9ff2c7056b4f2fc606337da9e962d48c92654346
KalyanaVarma <96328087+KalyanaVarma@users.noreply.github.com>
2022-02-05 19:09:27 +0530

    Update BackEnd.py

--- BackEnd.py
+++ BackEnd.py
@@ -8,7 +8,8 @@ import time
 
 import numpy as np
 
-from cv2 import cv2
+import cv2
+from .cv2 import *
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import File
@@ -120,8 +121,8 @@ def DetectRoadObjects(InputFile):
   Colors = np.random.randint(0, 255, size = (len(Labels), 3), dtype = "uint8")
   
   # Load YOLOv3 object detector trained on custom i.e. bdd100k dataset (Currently 3 classes i.e. Person, Vehicle, TrafficInfo).
-  # Network = cv2.dnn.readNetFromDarknet(PATH_CONFIG, PATH_WEIGHT)
-  Network = cv2.dnn.readNet(PATH_WEIGHT, PATH_CONFIG)
+  Network = cv2.dnn.readNetFromDarknet(PATH_CONFIG, PATH_WEIGHT)
+  # Network = cv2.dnn.readNet(PATH_WEIGHT, PATH_CONFIG)
 
 
   # Get the image height and image width values from the resized color image.
